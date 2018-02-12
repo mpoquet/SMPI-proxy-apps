59e0f89846fe86ca7685eb6f9cc04b63f5c26708
Martin Quinson <martin.quinson@loria.fr>
2018-02-12 01:21:37 +0100

    Let the mdtest example work on my machine

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+
+export SIMGRID_PATH=/builds/workspace/SimGrid-Multi/build_mode/Debug/node/simgrid-fedora-rawhide-64/build/SimGrid-3.19
+
 set -exu
 mkdir -p bin
 emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "main.org")'
